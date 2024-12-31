6411e833b627a0d5de113aa85fbd9eaf19f9ccb9
clut19 <vgonzalezseb@gmail.com>
2024-12-31 04:14:34 +0100

    Create getComputerChoice function
    Create the function that will randomly choose Paper, Rock or Scissors
    from the computer perspective.

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-let a = 5;
+function getComputerChoice(){
 
-console.log (a);
+    let a = Math.random();
+    let result;
+    if (a <= 0.33 ){
+        result = "Paper";
+
+    } else if (a >=0.66){
+        result = "Rock";
+    } else {
+        result = "Scissors";
+    }
+
+    return result;
+}
+
+console.log (getComputerChoice());
+console.log (getComputerChoice());
+console.log (getComputerChoice());
+console.log (getComputerChoice());
+console.log (getComputerChoice());
+console.log (getComputerChoice());
+console.log (getComputerChoice());
+console.log (getComputerChoice());
+console.log (getComputerChoice());
+console.log (getComputerChoice());
+console.log (getComputerChoice());
